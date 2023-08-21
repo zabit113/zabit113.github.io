d4d77fae6675116df46a4078058bbeb4b14583d7
zabit113 <shabvica@gmail.com>
2023-08-21 12:41:43 -0500

    asd

---
+++ js/galeria.js
@@ -0,0 +1,12 @@
+$('.galeria__img').click(function(e){
+	
+	var img=e.target.src;
+	
+	var modal ='<div class="modal" id="modal"><img src=" '+img+' " class="modal__img"><div class="modal__boton" id="modal__boton">X</div></div>';
+	
+	$('body').append(modal);
+	$('#modal__boton').click(function(){
+		$('#modal').remove();
+	});
+})
+
